f8fb91ebb381e8876341a1d60e4d1d175899549a
Justin Wong <justinwong12337@gmail.com>
2016-10-02 17:44:45 -0700

    Adding desktop navbar

--- js/directives/landing-directives.js
+++ js/directives/landing-directives.js
@@ -1,5 +1,10 @@
 //MAIN DIRECTIVE FOR MAIN TEMPLATE HTML
 
 define(['directivesFactory'], function(directivesFactory){
-
+  directivesFactory.directive('navbar', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'templates/partials/navbar.html'
+    };
+  });
 });
