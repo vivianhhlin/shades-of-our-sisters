19bc8174028e00d03949c75869354b7b39d60cd4
Justin Wong <justinwong12337@gmail.com>
2016-10-11 19:59:53 -0700

    Added hover features

--- js/services/content-service.js
+++ js/services/content-service.js
@@ -8,9 +8,9 @@ define(['servicesFactory'],
         var service = {
           /* ------------ LANDING PAGE --------------*/
 
-          profile_left_name: 'SONYA',
+          profile_left_name: 'Sonya',
           profile_left_description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-          profile_right_name: 'PATRICIA',
+          profile_right_name: 'Patricia',
           profile_right_description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
           blurb: 'This is the about section. To the right is Sonya, to the left is Patricia.',
         };
