3065b2c8a227fc08f606008301e58e0ad1cb182c
Justin Wong <justinwong12337@gmail.com>
2016-10-11 19:30:45 -0700

    Added content-services.js for content html binding

--- js/controllers/landing-ctrl.js
+++ js/controllers/landing-ctrl.js
@@ -2,13 +2,21 @@
 //CONTROLLER DEFINTIIONS SHOULD BE MINIMAL SPECIFYING WHICH CONTROLLER USES WHICH SERVICE, ETC.
 
 define(["controllersFactory",
+  'services/content-service',
   'directives/landing-directives'],
   function(controllersFactory) {
 
   controllersFactory.controller('LandingCtrl',
     ['$scope',
+     'ContentService',
 
     function($scope,
-      projectService) {
+      contentService) {
+
+      $scope.contentService = contentService.init($scope);
+
+      for (var key in contentService) {
+        $scope[key] = contentService[key];
+      }
   }]);
 });
